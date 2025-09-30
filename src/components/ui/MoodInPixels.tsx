eda3eca2b67feab366ea6eaccdc68b5d4b0de6b0
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-09-30 21:13:38 +0200

    refactor: consolidate MoodInPixels and MoodInPixelsGrid components

--- src/components/ui/MoodInPixels.tsx
+++ src/components/ui/MoodInPixels.tsx
@@ -1,29 +1,35 @@
+import { getMoodDataFromMood } from '../../utils/moods';
 import type { Entry } from '../../global/types';
 import Card from '../shared/Card';
-import MoodInPixelsGrid from './MoodInPixelsGrid';
 
-interface ColorGridProps {
+interface MoodInPixelsGridProps {
   entries: Entry[];
   setSelectedDate: (date: string) => void;
   selectedDate: string;
 }
 
-function MoodInPixels({
+export default function MoodInPixels({
   entries,
   setSelectedDate,
   selectedDate,
-}: ColorGridProps) {
-  if (!entries.length) return null;
-
+}: MoodInPixelsGridProps) {
   return (
-    <Card title='Mood In Pixels'>
-      <MoodInPixelsGrid
-        entries={entries}
-        setSelectedDate={setSelectedDate}
-        selectedDate={selectedDate}
-      />
+    <Card title='Mood in Pixels'>
+      <div className='flex flex-wrap gap-2 mt-2'>
+        {entries.map((entry) => (
+          <div className='tooltip' data-tip={entry.date} key={entry.date}>
+            <div
+              className={`w-6 h-6 rounded-sm cursor-pointer hover:scale-130 transition-transform ${
+                entry.date === selectedDate ? 'border-3 border-white' : ''
+              }`}
+              style={{
+                backgroundColor: getMoodDataFromMood(entry.mood)?.color,
+              }}
+              onClick={() => setSelectedDate(entry.date)}
+            ></div>
+          </div>
+        ))}
+      </div>
     </Card>
   );
 }
-
-export default MoodInPixels;
