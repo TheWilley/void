9c97180d78ff2e72a17b855c2623d8d7b2ad36ef
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-10-19 08:22:18 +0200

    feat: implement a grid layout for larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -21,45 +21,51 @@ export default function App() {
   } = useMood();
 
   return (
-    <div className='min-h-screen flex flex-col items-center justify-start p-6 gap-6 [&>*]:max-w-xl'>
-      <div className='font-mono'>
-        <h1 className='text-3xl font-bold'> {config.appName}</h1>
-        <h2 className='text-xs text-center'>{config.version}</h2>
+    <div className='min-h-screen flex items-center justify-center p-6 gap-6'>
+      <div className='w-2xl lg:w-7xl'>
+        <div className='flex flex-col justify-center text-center mb-5'>
+          <div className='font-mono mb-5'>
+            <h1 className='text-3xl font-bold'> {config.appName}</h1>
+            <h2 className='text-xs text-center'>{config.version}</h2>
+          </div>
+          <ThemeToggler />
+        </div>
+
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+          <Search
+            entries={sortedEntries}
+            onSearchResults={(results) => {
+              setSearchResults(results);
+            }}
+          />
+
+          {/* Mood Input Card */}
+          <MoodInput
+            selectedEntry={selectedEntry}
+            deleteEntry={deleteEntry}
+            updateEntry={updateEntry}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+          />
+
+          {/* Color Grid */}
+          <MoodInPixels
+            entries={searchResults}
+            setSelectedDate={setSelectedDate}
+            selectedDate={selectedDate}
+          />
+
+          {/* Data Management */}
+          <DataManagment
+            entries={sortedEntries}
+            onDataLoaded={(data) => {
+              setEntries(data);
+            }}
+          />
+        </div>
+
+        <Footer />
       </div>
-      <ThemeToggler />
-
-      <Search
-        entries={sortedEntries}
-        onSearchResults={(results) => {
-          setSearchResults(results);
-        }}
-      />
-
-      {/* Mood Input Card */}
-      <MoodInput
-        selectedEntry={selectedEntry}
-        deleteEntry={deleteEntry}
-        updateEntry={updateEntry}
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-      />
-
-      {/* Color Grid */}
-      <MoodInPixels
-        entries={searchResults}
-        setSelectedDate={setSelectedDate}
-        selectedDate={selectedDate}
-      />
-
-      {/* Data Management */}
-      <DataManagment
-        entries={sortedEntries}
-        onDataLoaded={(data) => {
-          setEntries(data);
-        }}
-      />
-
-      <Footer />
     </div>
   );
 }
